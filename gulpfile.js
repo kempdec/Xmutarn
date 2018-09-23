4292a80bd91cc0d3026c10369c6bf5c74c921291
Vinícius Lima <viniciusvsl@outlook.com>
2018-09-23 19:57:52 -0300

    Adiciona automação de tarefas com o Gulp.js

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const gulp = require("gulp"),
+  autoprefixer = require("gulp-autoprefixer"),
+  header = require("gulp-header"),
+  rename = require("gulp-rename"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps");
+
+// propriedades do pacote.
+const pkg = JSON.parse(require("fs").readFileSync("./package.json")),
+
+  date = new Date();
+
+const banner = `/*! Xmutarn v${pkg.version} (${pkg.repository.url}) | Copyright ${date.getFullYear()} ${pkg.author.name} | Licensed under MIT (${pkg.repository.url.replace(".git", "")}/blob/master/LICENSE) */\n`;
+
+// caminho do arquivo SASS principal.
+const mainSassFile = `./src/scss/${pkg.name}.scss`,
+  // caminho de distribuição dos arquivos CSS.
+  cssDistPath = "./dist/style",
+
+  autoprefixerOptions = "defaults";
+
+gulp.task("sass:expanded", () => gulp.src(mainSassFile)
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+  .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(header(banner))
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest(cssDistPath)));
+
+gulp.task("sass:min", () => gulp.src(mainSassFile)
+  .pipe(sourcemaps.init())
+  .pipe(rename((path) => { path.basename += ".min"; }))
+  .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+  .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(header(banner))
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest(cssDistPath)));
+
+gulp.task("sass", ["sass:expanded", "sass:min"]);
+
+gulp.task("copyFilesToDist", ["sass"], () => gulp.src("./dist/**/*")
+  .pipe(gulp.dest("./docs/assets")));
+
+gulp.task("default", ["copyFilesToDist"]);
