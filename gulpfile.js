bd9a75268e283a9d78ea31a4e846ea405e9cabb3
Vinícius Lima <viniciusvsl@outlook.com>
2018-10-03 16:15:52 -0300

    Adiciona uma nova tarefa

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,16 @@ const gulp = require("gulp"),
   header = require("gulp-header"),
   rename = require("gulp-rename"),
   sass = require("gulp-sass"),
-  sourcemaps = require("gulp-sourcemaps");
+  sourcemaps = require("gulp-sourcemaps"),
+  ts = require("gulp-typescript");
+
+const tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("ts", () => tsProject.src()
+  .pipe(sourcemaps.init())
+  .pipe(tsProject())
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest(tsProject.options.outDir)));
 
 // propriedades do pacote.
 const pkg = JSON.parse(require("fs").readFileSync("./package.json")),
@@ -43,4 +52,4 @@ gulp.task("sass", ["sass:expanded", "sass:min"]);
 gulp.task("copyFilesToDist", ["sass"], () => gulp.src("./dist/**/*")
   .pipe(gulp.dest("./docs/assets")));
 
-gulp.task("default", ["copyFilesToDist"]);
+gulp.task("default", ["ts", "copyFilesToDist"]);
