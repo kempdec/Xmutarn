ca1960095a8746c059198b5b49bbf166d2487e8e
Vinícius Lima <viniciusvsl@outlook.com>
2018-11-17 15:51:37 -0200

    Aprimora a tarefa "ts"

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require("gulp"),
   autoprefixer = require("gulp-autoprefixer"),
   header = require("gulp-header"),
+  ignore = require("gulp-ignore"),
   rename = require("gulp-rename"),
   sass = require("gulp-sass"),
   sourcemaps = require("gulp-sourcemaps"),
@@ -11,21 +12,23 @@ const gulp = require("gulp"),
 
 const tsProject = ts.createProject("./tsconfig.json");
 
-gulp.task("ts:compiled", () => tsProject.src()
-  .pipe(sourcemaps.init())
-  .pipe(tsProject())
-  .pipe(sourcemaps.write("."))
-  .pipe(gulp.dest(tsProject.options.outFile)));
-
-gulp.task("ts", ["ts:compiled"], () => gulp.src(tsProject.options.outFile)
-  .pipe(sourcemaps.init())
-  .pipe(rename((path) => { path.basename += ".min"; }))
-  .pipe(sourcemaps.write("."))
-  .pipe(gulp.dest(tsProject.options.outDir)));
-
 // propriedades do pacote.
 const pkg = JSON.parse(require("fs").readFileSync("./package.json"));
 const banner = `/*! Xmutarn v${pkg.version} (${pkg.repository.url}) | Copyright ${new Date().getFullYear()} ${pkg.author.name} | Licensed under MIT (${pkg.repository.url.replace(".git", "")}/blob/master/LICENSE) */\n`;
+
+gulp.task("ts", () => tsProject.src()
+  .pipe(sourcemaps.init())
+  .pipe(tsProject())
+  .pipe(header(banner))
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest(tsProject.options.outDir))
+  .pipe(ignore.exclude("*.map"))
+  .pipe(sourcemaps.init())
+  .pipe(uglify().on("error", e => console.log(e)))
+  .pipe(rename((path) => { path.basename += ".min"; }))
+  .pipe(header(banner))
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest(tsProject.options.outDir)));
 
 // caminho do arquivo SASS principal.
 const mainSassFile = `./src/scss/${pkg.name}.scss`,
@@ -42,7 +45,7 @@ gulp.task("sass:expanded", () => gulp.src(mainSassFile)
   .pipe(sourcemaps.write("."))
   .pipe(gulp.dest(cssDistPath)));
 
-gulp.task("sass", ["sass:expanded"], () => gulp.src(mainSassFile)
+gulp.task("sass:min", () => gulp.src(mainSassFile)
   .pipe(sourcemaps.init())
   .pipe(rename((path) => { path.basename += ".min"; }))
   .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
@@ -50,6 +53,8 @@ gulp.task("sass", ["sass:expanded"], () => gulp.src(mainSassFile)
   .pipe(header(banner))
   .pipe(sourcemaps.write("."))
   .pipe(gulp.dest(cssDistPath)));
+
+gulp.task("sass", ["sass:expanded", "sass:min"]);
 
 gulp.task("default", ["ts", "sass"], () => gulp.src("./dist/**/*")
   .pipe(gulp.dest("./docs/assets")));
