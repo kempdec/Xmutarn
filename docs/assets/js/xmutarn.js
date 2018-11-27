fd6d9af324141ffe7844df74aed9506cfdfd296a
Vinícius Lima <viniciusvsl@outlook.com>
2018-11-27 15:58:12 -0200

    Adiciona classe responsável por gerenciar um menu suspenso

--- docs/assets/js/xmutarn.js
+++ docs/assets/js/xmutarn.js
@@ -1,4 +1,139 @@
 /*! Xmutarn v0.3.0 (https://github.com/vinivsl/Xmutarn.git) | Copyright 2018 Vinícius Lima | Licensed under MIT (https://github.com/vinivsl/Xmutarn/blob/master/LICENSE) */
+var DropdownMenuOrigin;
+(function (DropdownMenuOrigin) {
+    DropdownMenuOrigin["TopRight"] = "top-right";
+    DropdownMenuOrigin["TopRightPartial"] = "top-right-partial";
+    DropdownMenuOrigin["TopLeft"] = "top-left";
+    DropdownMenuOrigin["TopLeftPartial"] = "top-left-partial";
+    DropdownMenuOrigin["TopStart"] = "top-start";
+    DropdownMenuOrigin["TopStartPartial"] = "top-start-partial";
+    DropdownMenuOrigin["TopEnd"] = "top-end";
+    DropdownMenuOrigin["TopEndPartial"] = "top-end-partial";
+    DropdownMenuOrigin["BottomRight"] = "bottom-right";
+    DropdownMenuOrigin["BottomRightPartial"] = "bottom-right-partial";
+    DropdownMenuOrigin["BottomLeft"] = "bottom-left";
+    DropdownMenuOrigin["BottomLeftPartial"] = "bottom-left-partial";
+    DropdownMenuOrigin["BottomStart"] = "bottom-start";
+    DropdownMenuOrigin["BottomStartPartial"] = "bottom-start-partial";
+    DropdownMenuOrigin["BottomEnd"] = "bottom-end";
+    DropdownMenuOrigin["BottomEndPartial"] = "bottom-end-partial";
+})(DropdownMenuOrigin || (DropdownMenuOrigin = {}));
+var DropdownMenu = (function () {
+    function DropdownMenu(element, origin) {
+        if (origin === void 0) { origin = DropdownMenuOrigin.TopRight; }
+        var _this = this;
+        this.element = element;
+        if (!element) {
+            throw new Error("O primeiro argumento deve ser fornecido.");
+        }
+        this.menu =
+            element.nextElementSibling.classList.contains("menu") ? element.nextElementSibling : undefined;
+        if (!this.menu) {
+            throw new Error("Nenhum elemento imediatamente próximo, com a classe 'menu', foi encontrado.");
+        }
+        if (origin != null) {
+            this.menu.classList.add("menu_origin-" + origin.toLowerCase().replace("-partial", ""));
+        }
+        this.menu.addEventListener("click", function (e) { return e.stopPropagation(); });
+        var definedOrigin = false;
+        element.addEventListener("click", function (e) {
+            _this.toggle();
+            if (definedOrigin === false && origin != null) {
+                _this.setOrigin(origin);
+                definedOrigin = true;
+            }
+            e.stopPropagation();
+            e.preventDefault();
+        });
+        document.addEventListener("click", function () { return _this.close(); });
+    }
+    DropdownMenu.prototype.setOrigin = function (origin) {
+        if (origin === void 0) { origin = DropdownMenuOrigin.TopRight; }
+        var top, left;
+        if (origin === DropdownMenuOrigin.TopRight ||
+            origin === DropdownMenuOrigin.TopLeft ||
+            origin === DropdownMenuOrigin.TopStart ||
+            origin === DropdownMenuOrigin.TopEnd) {
+            top = this.element.offsetTop + this.element.offsetHeight;
+        }
+        if (origin === DropdownMenuOrigin.TopRightPartial ||
+            origin === DropdownMenuOrigin.TopLeftPartial ||
+            origin === DropdownMenuOrigin.TopStartPartial ||
+            origin === DropdownMenuOrigin.TopEndPartial) {
+            top = this.element.offsetTop;
+        }
+        if (origin === DropdownMenuOrigin.BottomRight ||
+            origin === DropdownMenuOrigin.BottomLeft ||
+            origin === DropdownMenuOrigin.BottomStart ||
+            origin === DropdownMenuOrigin.BottomEnd) {
+            top = this.element.offsetTop - this.menu.offsetHeight;
+        }
+        if (origin === DropdownMenuOrigin.BottomRightPartial ||
+            origin === DropdownMenuOrigin.BottomLeftPartial ||
+            origin === DropdownMenuOrigin.BottomStartPartial ||
+            origin === DropdownMenuOrigin.BottomEndPartial) {
+            top = this.element.offsetTop - this.menu.offsetHeight + this.element.offsetHeight;
+        }
+        if (origin === DropdownMenuOrigin.TopRight ||
+            origin === DropdownMenuOrigin.TopRightPartial ||
+            origin === DropdownMenuOrigin.BottomRight ||
+            origin === DropdownMenuOrigin.BottomRightPartial) {
+            left = this.element.offsetLeft + (this.element.offsetWidth - this.menu.offsetWidth);
+        }
+        if (origin === DropdownMenuOrigin.TopLeft ||
+            origin === DropdownMenuOrigin.TopLeftPartial ||
+            origin === DropdownMenuOrigin.BottomLeft ||
+            origin === DropdownMenuOrigin.BottomLeftPartial) {
+            left = this.element.offsetLeft;
+        }
+        if (origin === DropdownMenuOrigin.TopStart ||
+            origin === DropdownMenuOrigin.TopStartPartial ||
+            origin === DropdownMenuOrigin.BottomStart ||
+            origin === DropdownMenuOrigin.BottomStartPartial) {
+            left = this.element.offsetLeft - this.menu.offsetWidth;
+        }
+        if (origin === DropdownMenuOrigin.TopEnd ||
+            origin === DropdownMenuOrigin.TopEndPartial ||
+            origin === DropdownMenuOrigin.BottomEnd ||
+            origin === DropdownMenuOrigin.BottomEndPartial) {
+            left = this.element.offsetLeft + this.element.offsetHeight;
+        }
+        this.menu.style.top = top + "px";
+        this.menu.style.left = left + "px";
+        return this.element;
+    };
+    DropdownMenu.prototype.open = function (origin) {
+        if (this.element.classList.contains("dropdown")) {
+            this.element.classList.add("dropdown_active");
+        }
+        if (origin != null) {
+            this.setOrigin(origin);
+        }
+        this.menu.classList.add("menu_open");
+        return this.element;
+    };
+    DropdownMenu.prototype.close = function () {
+        if (this.element.classList.contains("dropdown")) {
+            this.element.classList.remove("dropdown_active");
+        }
+        this.menu.classList.remove("menu_open");
+        return this.element;
+    };
+    DropdownMenu.prototype.toggle = function (origin) {
+        if (this.element.classList.contains("dropdown")) {
+            this.element.classList.toggle("dropdown_active");
+        }
+        if (origin != null) {
+            this.setOrigin(origin);
+        }
+        this.menu.classList.toggle("menu_open");
+        return this.element;
+    };
+    return DropdownMenu;
+}());
+document.querySelectorAll("[x-role='dropdown-menu']").forEach(function (element) {
+    new DropdownMenu(element, element.getAttribute("x-origin"));
+});
 var Input = (function () {
     function Input(element, labelText) {
         var _this = this;
---
+++ src/ts/dropdownMenu.ts
@@ -0,0 +1,219 @@
+/**
+ * Representa a origem de abertura do menu suspenso.
+ */
+enum DropdownMenuOrigin {
+  TopRight = "top-right",
+  TopRightPartial = "top-right-partial",
+  TopLeft = "top-left",
+  TopLeftPartial = "top-left-partial",
+  TopStart = "top-start",
+  TopStartPartial = "top-start-partial",
+  TopEnd = "top-end",
+  TopEndPartial = "top-end-partial",
+  BottomRight = "bottom-right",
+  BottomRightPartial = "bottom-right-partial",
+  BottomLeft = "bottom-left",
+  BottomLeftPartial = "bottom-left-partial",
+  BottomStart = "bottom-start",
+  BottomStartPartial = "bottom-start-partial",
+  BottomEnd = "bottom-end",
+  BottomEndPartial = "bottom-end-partial"
+}
+
+/**
+ * Responsável por gerenciar um menu suspenso.
+ */
+class DropdownMenu {
+  /** Obtém ou define o menu suspenso. */
+  public menu: HTMLElement;
+
+  /**
+   * Inicializa uma nova instância de DropdownMenu.
+   *
+   * @param {HTMLElement} element O elemento responsável por abrir o menu suspenso.
+   * @param {DropdownMenuOrigin} origin A origem da transformação do menu suspenso. O padrão é
+   * "DropdownMenuOrigin.TopRight".
+   */
+  constructor(public element: HTMLElement, origin: DropdownMenuOrigin = DropdownMenuOrigin.TopRight) {
+    if (!element) {
+      throw new Error("O primeiro argumento deve ser fornecido.");
+    }
+
+    this.menu =
+      element.nextElementSibling.classList.contains("menu") ? <HTMLElement>element.nextElementSibling : undefined;
+
+    if (!this.menu) {
+      throw new Error("Nenhum elemento imediatamente próximo, com a classe 'menu', foi encontrado.");
+    }
+
+    // adiciona a classe que irá definir a origem da abetura do menu suspenso, se o parâmetro "origin" não for nulo.
+    if (origin != null) {
+      this.menu.classList.add(`menu_origin-${origin.toLowerCase().replace("-partial", "")}`);
+    }
+
+    this.menu.addEventListener("click", e => e.stopPropagation());
+
+    // sinalizador indicando se a origem foi definida.
+    let definedOrigin: boolean = false;
+
+    element.addEventListener("click", e => {
+      this.toggle();
+
+      if (definedOrigin === false && origin != null) {
+        this.setOrigin(origin);
+
+        definedOrigin = true;
+      }
+
+      e.stopPropagation();
+      e.preventDefault();
+    });
+
+    document.addEventListener("click", () => this.close());
+  }
+
+  /**
+   * Define a origem da transformação do menu suspenso.
+   *
+   * @param {DropdownMenuOrigin} origin A origem da transformação do menu suspenso. O padrão é
+   * "DropdownMenuOrigin.Topright".
+   *
+   * @return {HTMLElement} O elemento responsável por abrir o menu suspenso.
+   */
+  public setOrigin(origin: DropdownMenuOrigin = DropdownMenuOrigin.TopRight): HTMLElement {
+    let top: number,
+      left: number;
+
+    // top.
+    if (origin === DropdownMenuOrigin.TopRight ||
+      origin === DropdownMenuOrigin.TopLeft ||
+      origin === DropdownMenuOrigin.TopStart ||
+      origin === DropdownMenuOrigin.TopEnd) {
+      top = this.element.offsetTop + this.element.offsetHeight;
+    }
+
+    // top partial.
+    if (origin === DropdownMenuOrigin.TopRightPartial ||
+      origin === DropdownMenuOrigin.TopLeftPartial ||
+      origin === DropdownMenuOrigin.TopStartPartial ||
+      origin === DropdownMenuOrigin.TopEndPartial) {
+      top = this.element.offsetTop;
+    }
+
+    // bottom.
+    if (origin === DropdownMenuOrigin.BottomRight ||
+      origin === DropdownMenuOrigin.BottomLeft ||
+      origin === DropdownMenuOrigin.BottomStart ||
+      origin === DropdownMenuOrigin.BottomEnd) {
+      top = this.element.offsetTop - this.menu.offsetHeight;
+    }
+
+    // bottom partial.
+    if (origin === DropdownMenuOrigin.BottomRightPartial ||
+      origin === DropdownMenuOrigin.BottomLeftPartial ||
+      origin === DropdownMenuOrigin.BottomStartPartial ||
+      origin === DropdownMenuOrigin.BottomEndPartial) {
+      top = this.element.offsetTop - this.menu.offsetHeight + this.element.offsetHeight;
+    }
+
+    // right, right partial.
+    if (origin === DropdownMenuOrigin.TopRight ||
+      origin === DropdownMenuOrigin.TopRightPartial ||
+      origin === DropdownMenuOrigin.BottomRight ||
+      origin === DropdownMenuOrigin.BottomRightPartial) {
+      left = this.element.offsetLeft + (this.element.offsetWidth - this.menu.offsetWidth);
+    }
+
+    // left, left partial.
+    if (origin === DropdownMenuOrigin.TopLeft ||
+      origin === DropdownMenuOrigin.TopLeftPartial ||
+      origin === DropdownMenuOrigin.BottomLeft ||
+      origin === DropdownMenuOrigin.BottomLeftPartial) {
+      left = this.element.offsetLeft;
+    }
+
+    // start, start partial.
+    if (origin === DropdownMenuOrigin.TopStart ||
+      origin === DropdownMenuOrigin.TopStartPartial ||
+      origin === DropdownMenuOrigin.BottomStart ||
+      origin === DropdownMenuOrigin.BottomStartPartial) {
+      left = this.element.offsetLeft - this.menu.offsetWidth;
+    }
+
+    // end, end partial.
+    if (origin === DropdownMenuOrigin.TopEnd ||
+      origin === DropdownMenuOrigin.TopEndPartial ||
+      origin === DropdownMenuOrigin.BottomEnd ||
+      origin === DropdownMenuOrigin.BottomEndPartial) {
+      left = this.element.offsetLeft + this.element.offsetHeight;
+    }
+
+    // define o topo e a esquerda do menu suspenso.
+    this.menu.style.top = top + "px";
+    this.menu.style.left = left + "px";
+
+    return this.element;
+  }
+
+  /**
+   * Abre o menu suspenso.
+   *
+   * @param {DropdownMenuOrigin} origin A origem da transformação do menu suspenso. O padrão é
+   * "DropdownMenuOrigin.TopRight".
+   *
+   * @return {HTMLElement} O elemento responsável por abrir o menu dropdown.
+   */
+  public open(origin?: DropdownMenuOrigin): HTMLElement {
+    if (this.element.classList.contains("dropdown")) {
+      this.element.classList.add("dropdown_active");
+    }
+
+    if (origin != null) {
+      this.setOrigin(origin);
+    }
+
+    this.menu.classList.add("menu_open");
+
+    return this.element;
+  }
+
+  /**
+   * Fecha o menu suspenso.
+   *
+   * @return {HTMLElement} O elemento responsável por abrir o menu suspenso.
+   */
+  public close(): HTMLElement {
+    if (this.element.classList.contains("dropdown")) {
+      this.element.classList.remove("dropdown_active");
+    }
+
+    this.menu.classList.remove("menu_open");
+
+    return this.element;
+  }
+
+  /**
+   * Alterna entre abrir e fechar o menu suspenso.
+   *
+   * @param {DropdownMenuOrigin} origin A origem da transformação do menu suspenso. O padrão é
+   * "DropdownMenuOrigin.TopRight".
+   */
+  public toggle(origin?: DropdownMenuOrigin): HTMLElement {
+    if (this.element.classList.contains("dropdown")) {
+      this.element.classList.toggle("dropdown_active");
+    }
+
+    if (origin != null) {
+      this.setOrigin(origin);
+    }
+
+    this.menu.classList.toggle("menu_open");
+
+    return this.element;
+  }
+}
+
+// inicializa uma nova instância de DropdownMenu, a partir do atributo HTML "x-dropdown-menu".
+document.querySelectorAll("[x-role='dropdown-menu']").forEach((element: HTMLElement): void => {
+  new DropdownMenu(element, <DropdownMenuOrigin>element.getAttribute("x-origin"));
+});
