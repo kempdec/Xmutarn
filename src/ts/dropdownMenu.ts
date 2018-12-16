b22f538f43fcf514812de73559993bba63c4eacc
Vinícius Lima <viniciusvsl@outlook.com>
2018-12-16 17:12:20 -0200

    Separa os enumeradores dos arquivos de classes

--- src/ts/dropdownMenu.ts
+++ src/ts/dropdownMenu.ts
@@ -1,24 +1,4 @@
-/**
- * Representa a origem de abertura do menu suspenso.
- */
-enum DropdownMenuOrigin {
-  TopRight = "top-right",
-  TopRightPartial = "top-right-partial",
-  TopLeft = "top-left",
-  TopLeftPartial = "top-left-partial",
-  TopStart = "top-start",
-  TopStartPartial = "top-start-partial",
-  TopEnd = "top-end",
-  TopEndPartial = "top-end-partial",
-  BottomRight = "bottom-right",
-  BottomRightPartial = "bottom-right-partial",
-  BottomLeft = "bottom-left",
-  BottomLeftPartial = "bottom-left-partial",
-  BottomStart = "bottom-start",
-  BottomStartPartial = "bottom-start-partial",
-  BottomEnd = "bottom-end",
-  BottomEndPartial = "bottom-end-partial"
-}
+/// <reference path="dropdownMenuOrigin.ts" />
 
 /**
  * Responsável por gerenciar um menu suspenso.
--- src/ts/toast.ts
+++ src/ts/toast.ts
@@ -1,22 +1,4 @@
-/**
- * Representa a cor do toast.
- */
-enum ToastColor {
-  /** Cor destaque do tema. */
-  FeaturedColor = "featured",
-
-  /** Cor de realçe do tema. */
-  AccentColor = "accent",
-
-  /** Cor de sucesso. */
-  SuccessColor = "success",
-
-  /** Cor de alerta. */
-  AlertColor = "alert",
-
-  /** Cor de cuidado. */
-  WarningColor = "warning"
-}
+/// <reference path="toastColor.ts" />
 
 /**
  * Responsável por gerenciar um toast.
