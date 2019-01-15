dbbb6fd08262b1a9e06192fc580af6b7b78c8d32
Vinícius Lima <viniciusvsl@outlook.com>
2019-01-14 22:01:59 -0200

    Adiciona Vue e move Prism de diretório

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const { dest } = require("gulp"),
+const { src, dest } = require("gulp"),
+  merge = require("merge-stream"),
+  rimraf = require("rimraf"),
   sourcemaps = require("gulp-sourcemaps"),
   typescript = require("gulp-typescript");
 
@@ -17,5 +19,44 @@ function ts() {
     .pipe(dest(tsProject.options.outDir));
 }
 
+/** Diretório destino das bibliotecas. */
+const libPath = "./assets/lib";
+
+/** Dependências e seus diretórios (fonte e destino). */
+const dependencies = {
+  "vue": {
+    "dist/*": "dist/"
+  }
+};
+
+/**
+ * Copia os pacotes de dependências para os ativos da documentação.
+ */
+function copyPackagesToDocs(cb) {
+  // apaga todos os arquivos existentes no diretório destino das bibliotecas.
+  rimraf(libPath, cb);
+
+  const streams = [];
+
+  for (let dependency in dependencies) {
+    console.log("Movendo dependência: " + dependency);
+
+    for (let path in dependencies[dependency]) {
+      // diretório da dependência.
+      const dependencyPath = `../node_modules/${dependency}/${path}`;
+
+      // diretório destino da dependência.
+      const dependencyDestPath = `${libPath}/${dependency}/${dependencies[dependency][path]}`;
+
+      streams.push(
+        src(dependencyPath)
+          .pipe(dest(dependencyDestPath)));
+    }
+  }
+
+  return merge(streams);
+}
+
 exports.ts = ts;
+exports.copyPackagesToDocs = copyPackagesToDocs;
 exports.default = ts;
