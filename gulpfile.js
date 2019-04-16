98cebe97faeb7a6ba23a91d5df46ae22135e1fc8
Vinícius Lima <viniciusvsl@outlook.com>
2019-04-16 17:30:33 -0300

    Refatorar

--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,21 @@ const header = require("gulp-header");
 const rename = require("gulp-rename");
 const sourcemaps = require("gulp-sourcemaps");
 
-/** Propriedades do pacote. */
+/** Propriedades do pacote de dependências. */
 const pkg = JSON.parse(require("fs").readFileSync("package.json"));
 
+/** Cabeçalho dos arquivos de distribuição. */
 const banner = `/*! Xmutarn v${pkg.version} (${pkg.repository.url}) | Copyright ${new Date().getFullYear()} ${pkg.author.name} | Licensed under MIT (${pkg.repository.url.replace(".git", "")}/blob/master/LICENSE) */\n`;
 
+/** Caminhos. */
 const paths = {
-
   /** Caminho de distribuição dos arquivos CSS. */
-  cssDistPath: "dist/css",
-
+  cssDist: "dist/css",
   /** Caminho de distribuição dos arquivos JS. */
   jsDist: "dist/js"
-
 };
 
-const sassTask = () => {
+function sass() {
   const autoprefixer = require("gulp-autoprefixer");
   const sass = require("gulp-sass");
 
@@ -39,7 +38,7 @@ const sassTask = () => {
     .pipe(header(banner))
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write("."))
-    .pipe(dest(paths.cssDistPath));
+    .pipe(dest(paths.cssDist));
 
   return src(mainSassFile)
     .pipe(rename((path) => { path.basename += ".min"; }))
@@ -48,16 +47,18 @@ const sassTask = () => {
     .pipe(header(banner))
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write("."))
-    .pipe(dest(paths.cssDistPath));
-};
+    .pipe(dest(paths.cssDist));
+}
 
-const copyToDocs = () =>
-  src("dist/**/*")
+/** Copia os arquivos de distribuição para os ativos da documentação do projeto. */
+function copyToDocs() {
+  return src("dist/**/*")
     .pipe(dest("docs/assets"));
+}
 
-exports.sass = series(sassTask, copyToDocs);
+exports.sass = series(sass, copyToDocs);
 
-const ts = () => {
+function ts() {
 
   const browserify = require("browserify");
   const buffer = require("vinyl-buffer");
@@ -73,7 +74,7 @@ const ts = () => {
   })
     .plugin(tsify)
     .bundle().on("error", e => console.error(e))
-    .pipe(source("xmutarn-md.js"))
+    .pipe(source(`${pkg.name}-md.js`))
     .pipe(header(banner))
     .pipe(dest(paths.jsDist))
     .pipe(buffer())
@@ -87,4 +88,4 @@ const ts = () => {
 
 exports.ts = series(ts, copyToDocs);
 
-exports.default = series(parallel(ts, sassTask), copyToDocs);
+exports.default = series(parallel(ts, sass), copyToDocs);
