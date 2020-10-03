63ccc71d5ad381b24d2c9b0969783f44e2045f10
Vinícius Lima <viniciusxdl@outlook.com>
2020-10-02 23:43:56 -0300

    Atualizar tarefa 'css'

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,64 +1,13 @@
-import { dest, parallel, series, src, task } from "gulp";
+import { parallel, series, task } from "gulp";
 
-import { fileHeader } from "./scripts/gulp/header";
-import { buildDocs, buildJs, copyDocsDependency, delCss, delDocs, delDocsLibs, delJs, watchDocs } from "./scripts/gulp";
-
-/** Os caminhos do projeto. */
-const paths = {
-
-  /** Caminho dos arquivos de distribuição de folhas de estilo CSS do projeto. */
-  css: "dist/css"
-};
-
-const header = require("gulp-header");
-const rename = require("gulp-rename");
-const sourcemaps = require("gulp-sourcemaps");
-
-/** Constrói os arquivos de distribuição de folhas de estilo CSS do projeto. */
-function buildCss() {
-
-  const autoprefixer = require("gulp-autoprefixer");
-  const sass = require("gulp-sass");
-
-  /** Arquivo principal do SASS. */
-  const mainSassFile = `src/scss/xmutarn*.scss`;
-
-  const autoprefixerOptions = "defaults";
-
-  src(mainSassFile)
-    .pipe(sass({
-      outputStyle: "expanded"
-    })).on("error", sass.logError)
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(header(fileHeader))
-    .pipe(sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe(sourcemaps.write("."))
-    .pipe(dest(paths.css));
-
-  return src(mainSassFile)
-    .pipe(rename((path: any) => {
-      path.basename += ".min";
-    }))
-    .pipe(sass({
-      outputStyle: "compressed"
-    })).on("error", sass.logError)
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(header(fileHeader))
-    .pipe(sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe(sourcemaps.write("."))
-    .pipe(dest(paths.css));
-}
+import { buildCss, buildDocs, buildJs, copyDocsDependency, delCss, delDocs, delDocsLibs, delJs, watchDocs } from "./scripts/gulp";
 
 task("css", series(delCss, buildCss));
-
 task("js", series(delJs, buildJs));
 
 task("docs", series(delDocs, buildDocs));
 task("docsDependency", series(delDocsLibs, copyDocsDependency));
+task("cssDocs", series("css", "docsDependency"));
 task("jsDocs", series("js", "docsDependency"));
 task("watchDocs", watchDocs);
 
--- scripts/gulp/css.ts
+++ scripts/gulp/css.ts
@@ -1,13 +1,48 @@
+import autoprefixer from "gulp-autoprefixer";
 import del from "del";
+import header from "gulp-header";
+import rename from "gulp-rename";
+import sass from "gulp-sass";
+import sourcemaps from "gulp-sourcemaps";
+import { dest, src } from "gulp";
+
+import { fileHeader } from "./header";
 
 const paths = {
 
     /** O caminho de distribuição das folhas CSS do projeto. */
-    distCss: "dist/css"
+    distCss: "dist/css",
+
+    /** O caminho dos arquivos SASS/SCSS do projeto. */
+    scssFiles: "src/scss/xmutarn*.scss"
 };
 
 /** Deleta o caminho de distribuição das folhas CSS do projeto. */
 export function delCss(): Promise<string[]> {
 
     return del(paths.distCss);
+}
+
+/** Constrói os arquivos de folhas CSS do projeto. */
+export function buildCss(): NodeJS.ReadWriteStream {
+
+    const scss = src(paths.scssFiles);
+
+    scss.pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(autoprefixer())
+        .pipe(header(fileHeader))
+        .pipe(sourcemaps.write("."))
+        .pipe(dest(paths.distCss));
+
+    const minCss = src(paths.scssFiles)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(rename(path => path.basename += ".min"))
+        .pipe(autoprefixer())
+        .pipe(header(fileHeader))
+        .pipe(sourcemaps.write("."))
+        .pipe(dest(paths.distCss));
+
+    return minCss;
 }
