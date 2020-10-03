4aaa0a81ee09520e453664572564606627f0caa1
Vinícius Lima <viniciusxdl@outlook.com>
2020-10-02 23:51:45 -0300

    Atualizar gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,18 @@
 import { parallel, series, task } from "gulp";
 
-import { buildCss, buildDocs, buildJs, copyDocsDependency, delCss, delDocs, delDocsLibs, delJs, watchDocs } from "./scripts/gulp";
+import {
+    buildCss, buildDocs, buildJs, copyDocsDependency, delCss, delDocs, delDocsLibs, delJs, watchDocs
+} from "./scripts/gulp";
 
 task("css", series(delCss, buildCss));
 task("js", series(delJs, buildJs));
 
 task("docs", series(delDocs, buildDocs));
 task("docsDependency", series(delDocsLibs, copyDocsDependency));
+
 task("cssDocs", series("css", "docsDependency"));
 task("jsDocs", series("js", "docsDependency"));
+
 task("watchDocs", watchDocs);
 
 task("default", series(parallel("css", "js", "docs"), "docsDependency"));
