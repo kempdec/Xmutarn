d4477138c58fdf8fb0926dbfbbbd1f33b5d8d695
Vinícius Lima <viniciusxdl@outlook.com>
2024-02-12 22:57:22 -0300

    Adicionar Badynet.

---
+++ tasks/Xmutarn.Badynet/Program.cs
@@ -0,0 +1,17 @@
+﻿using KempDec.Xmutarn;
+using KempDec.Xmutarn.Core;
+
+var xmutarn = new XmutarnCSS();
+
+WriteCSSTo(xmutarn, @"D:/Xmutarn/src/Xmutarn/wwwroot/css/xmutarn.css");
+
+var xmutarnMin = new XmutarnCSS(isMinified: true);
+
+WriteCSSTo(xmutarnMin, @"D:/Xmutarn/src/Xmutarn/wwwroot/css/xmutarn.min.css");
+
+static void WriteCSSTo(CSS css, string filePath)
+{
+    using var writer = new StreamWriter(filePath);
+
+    writer.WriteLine(css);
+}
