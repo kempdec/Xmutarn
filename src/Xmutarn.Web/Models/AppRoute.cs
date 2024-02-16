6919a6444b6f2e20ad4d9b21a146c87b8a08d18b
Vinícius Lima <viniciusxdl@outlook.com>
2024-02-16 17:16:09 -0300

    Refatorar.

---
+++ src/Xmutarn.Web/Models/AppRoute.cs
@@ -0,0 +1,10 @@
+﻿using KempDec.StarterDotNet.AppRoutes;
+
+namespace Xmutarn.Web.Models;
+
+/// <summary>
+/// Fornece instâncias <see cref="IAppRoute"/> pré-construídas que podem ser usadas para definir uma rota do aplicativo.
+/// </summary>
+public static class AppRoute
+{
+}
